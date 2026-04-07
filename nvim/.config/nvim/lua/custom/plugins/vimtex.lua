fbd84167af739b366eaf1cd82c3aafdfe2c89e01
Orestis Konstantinidis <30441437+orestkon@users.noreply.github.com>
2026-04-07 04:17:28 +0300

    Nvim: Fixing Vimtex

--- nvim/.config/nvim/lua/custom/plugins/vimtex.lua
+++ nvim/.config/nvim/lua/custom/plugins/vimtex.lua
@@ -7,15 +7,19 @@ return {
 			vim.g.vimtex_view_method = "skim"
 			vim.g.vimtex_view_skim_activate = 0
 			vim.g.vimtex_view_skim_sync = 1
+			vim.g.vimtex_view_automatic = 1
 
 			vim.g.vimtex_compiler_latexmk = {
+				executable = "latexmk",
 				engine = "xelatex",
+				build_dir = "build",
 				options = {
 					"-shell-escape",
 					"-verbose",
 					"-file-line-error",
 					"-synctex=1",
 					"-interaction=nonstopmode",
+					"-outdir=build",
 				},
 			}
 		end,
